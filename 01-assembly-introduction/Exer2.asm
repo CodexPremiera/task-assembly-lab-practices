b4d55c53a3af1ec8c4f25682acdf7f7e3abfdcbf
CodexPremiera <ashleyken.professional@gmail.com>
2024-09-05 10:59:44 +0800

    complete exercise 2

--- 01-assembly-introduction/Exer2.asm
+++ 01-assembly-introduction/Exer2.asm
@@ -435,6 +435,7 @@ start:
     mov dl, cl
     int 21h
 
+
 ; DISPLAY MY COLLEGE
     ; display C
     mov ah, 02h
@@ -558,6 +559,309 @@ start:
     mov dl, cl
     int 21h
     
+    ; display S
+    mov ah, 02h
+    mov cl, 53h
+    mov dl, cl
+    int 21h
+
+    ; display t
+    mov ah, 02h
+    mov cl, 74h
+    mov dl, cl
+    int 21h
+
+    ; display u
+    mov ah, 02h
+    mov cl, 75h
+    mov dl, cl
+    int 21h
+
+    ; display d
+    mov ah, 02h
+    mov cl, 64h
+    mov dl, cl
+    int 21h
+
+    ; display i
+    mov ah, 02h
+    mov cl, 69h
+    mov dl, cl
+    int 21h
+
+    ; display e
+    mov ah, 02h
+    mov cl, 65h
+    mov dl, cl
+    int 21h
+    
+    ; display s
+    mov ah, 02h
+    mov cl, 73h
+    mov dl, cl
+    int 21h
+
+    ; display ENTER
+    mov ah, 02h
+    mov cl, 0Ah
+    mov dl, cl
+    int 21h
+
+
+; DISPLAY MY UNIVERSITY
+
+    ; display C
+    mov ah, 02h
+    mov cl, 43h
+    mov dl, cl
+    int 21h
+
+    ; display e
+    mov ah, 02h
+    mov cl, 65h
+    mov dl, cl
+    int 21h
+
+    ; display b
+    mov ah, 02h
+    mov cl, 62h
+    mov dl, cl
+    int 21h
+    
+    ; display u
+    mov ah, 02h
+    mov cl, 75h
+    mov dl, cl
+    int 21h
+
+    ; display SPACE
+    mov ah, 02h
+    mov cl, 20h
+    mov dl, cl
+    int 21h
+
+    ; display I
+    mov ah, 02h
+    mov cl, 49h
+    mov dl, cl
+    int 21h
+
+    ; display n
+    mov ah, 02h
+    mov cl, 6Eh
+    mov dl, cl
+    int 21h
+    
+    ; display s
+    mov ah, 02h
+    mov cl, 73h
+    mov dl, cl
+    int 21h
+
+    ; display t
+    mov ah, 02h
+    mov cl, 74h
+    mov dl, cl
+    int 21h
+
+    ; display i
+    mov ah, 02h
+    mov cl, 69h
+    mov dl, cl
+    int 21h
+
+    ; display t
+    mov ah, 02h
+    mov cl, 74h
+    mov dl, cl
+    int 21h
+    
+    ; display u
+    mov ah, 02h
+    mov cl, 75h
+    mov dl, cl
+    int 21h
+
+    ; display t
+    mov ah, 02h
+    mov cl, 74h
+    mov dl, cl
+    int 21h
+    
+    ; display e
+    mov ah, 02h
+    mov cl, 65h
+    mov dl, cl
+    int 21h
+
+    ; display SPACE
+    mov ah, 02h
+    mov cl, 20h
+    mov dl, cl
+    int 21h
+    
+    ; display o
+    mov ah, 02h
+    mov cl, 6Fh
+    mov dl, cl
+    int 21h
+
+    ; display f
+    mov ah, 02h
+    mov cl, 66h
+    mov dl, cl
+    int 21h
+
+    ; display SPACE
+    mov ah, 02h
+    mov cl, 20h
+    mov dl, cl
+    int 21h
+
+    ; display T
+    mov ah, 02h
+    mov cl, 54h
+    mov dl, cl
+    int 21h
+    
+    ; display e
+    mov ah, 02h
+    mov cl, 65h
+    mov dl, cl
+    int 21h
+
+    ; display c
+    mov ah, 02h
+    mov cl, 63h
+    mov dl, cl
+    int 21h
+    
+    ; display h
+    mov ah, 02h
+    mov cl, 68h
+    mov dl, cl
+    int 21h
+
+    ; display n
+    mov ah, 02h
+    mov cl, 6Eh
+    mov dl, cl
+    int 21h
+    
+    ; display o
+    mov ah, 02h
+    mov cl, 6Fh
+    mov dl, cl
+    int 21h
+
+    ; display l
+    mov ah, 02h
+    mov cl, 6Ch
+    mov dl, cl
+    int 21h
+    
+    ; display o
+    mov ah, 02h
+    mov cl, 6Fh
+    mov dl, cl
+    int 21h
+
+    ; display g
+    mov ah, 02h
+    mov cl, 67h
+    mov dl, cl
+    int 21h
+
+    ; display y
+    mov ah, 02h
+    mov cl, 79h
+    mov dl, cl
+    int 21h
+
+    ; display SPACE
+    mov ah, 02h
+    mov cl, 20h
+    mov dl, cl
+    int 21h
+
+    ; display -
+    mov ah, 02h
+    mov cl, 2Dh
+    mov dl, cl
+    int 21h
+
+    ; display SPACE
+    mov ah, 02h
+    mov cl, 20h
+    mov dl, cl
+    int 21h
+
+    ; display U
+    mov ah, 02h
+    mov cl, 55h
+    mov dl, cl
+    int 21h
+
+    ; display n
+    mov ah, 02h
+    mov cl, 6Eh
+    mov dl, cl
+    int 21h
+
+    ; display i
+    mov ah, 02h
+    mov cl, 69h
+    mov dl, cl
+    int 21h
+
+    ; display v
+    mov ah, 02h
+    mov cl, 76h
+    mov dl, cl
+    int 21h
+
+    ; display e
+    mov ah, 02h
+    mov cl, 65h
+    mov dl, cl
+    int 21h
+
+    ; display r
+    mov ah, 02h
+    mov cl, 76h
+    mov dl, cl
+    int 21h
+
+    ; display s
+    mov ah, 02h
+    mov cl, 73h
+    mov dl, cl
+    int 21h
+
+    ; display i
+    mov ah, 02h
+    mov cl, 69h
+    mov dl, cl
+    int 21h
+
+    ; display t
+    mov ah, 02h
+    mov cl, 74h
+    mov dl, cl
+    int 21h
+
+    ; display y
+    mov ah, 02h
+    mov cl, 79h
+    mov dl, cl
+    int 21h
+
+    ; display ENTER
+    mov ah, 02h
+    mov cl, 0Ah
+    mov dl, cl
+    int 21h
+
     
 int 27h
 end start
